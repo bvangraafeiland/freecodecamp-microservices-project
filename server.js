c0ea2a3fdb7f59352cda1bcda13a4889d7cd0d7f
Bastiaan van Graafeiland <bastiaan89@live.nl>
2020-05-13 15:31:11 +0200

    add timestamps endpoint

--- server.js
+++ server.js
@@ -21,8 +21,13 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  const dateString = req.params.date_string;
+  const unix = Date.parse(dateString || new Date())
+  const date = new Date(unix || Number(dateString));
+  console.log(date);
+  const response = unix || date.getTime() ? { utc: date.toUTCString(), unix: date.getTime() } : { error: 'Invalid Date' };
+  res.json(response);
 });
 
 
